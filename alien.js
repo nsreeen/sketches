3088239cd6348023a45980f55cdcacb0dd2789ce
Nasreen </>
2022-09-23 16:43:45 +0100

    Improve alien

--- alien.js
+++ alien.js
@@ -1,10 +1,14 @@
-let moveAmount = 5;
+let moveAmount = 10;
 let boardSize = 400;
 let objects = [];
+let img;
+let isHome = false;
+let fadeIn = 0;
 
 function setup() {
   createCanvas(boardSize, boardSize);
   objects.push(["👾", 200, 200], ["🚀", 100, 300], ["🪐", 300, 100], ["🪐", 50, 150], ["☄️", 150, 50], ["☄️", 300, 300])
+  img = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/200px-Mercury_in_color_-_Prockter07-edit1.jpg");
 }
 
 function draw() {
@@ -14,7 +18,8 @@ function draw() {
   textSize(20);
   let collision = detectCollision();
   if (collision && objects[collision][0] == "🚀") {
-    text("Time to go home! 🚀🚀🚀", 10,200);
+    text("Time to go home! 🚀🚀🚀", 60,200);
+    goHome();
   }
   if (collision && objects[collision][0] == "☄️") {
     text("Comet is not your friend", 80,200);
@@ -24,6 +29,26 @@ function draw() {
   for (var j=objects.length-1; j>=0; j--) {
     text(objects[j][0], objects[j][1], objects[j][2])
   }
+
+  if (isHome) {
+    tint(255, fadeIn);
+    image(img, 0, 0, boardSize, boardSize);
+    tint(255, 255);
+    text(objects[0][0], objects[0][1], objects[0][2]);
+    fadeIn = fadeIn < 255 ? fadeIn + 1 : fadeIn;
+
+  }
+
+
+}
+
+function goHome(n) {
+  console.log("gohome")
+  return new Promise(resolve => {
+    setTimeout(() => {
+      isHome = true;
+    }, 300);
+  });
 }
 
 function keyPressed() {
